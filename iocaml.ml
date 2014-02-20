17b4713cce7b1d7d6ea1776ba1504374cd7be130
andrewray <evilkidder@gmail.com>
2014-02-20 23:48:32 +0000

    add back open Js

--- iocaml.ml
+++ iocaml.ml
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *)
 
+open Js
+
 let split_primitives p =
     let len = String.length p in
     let rec split beg cur =
